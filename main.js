155e6088ee96883959a965a81ab11f7bb570a29c
Godalo <81340015+Godalo@users.noreply.github.com>
2021-08-21 16:42:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function setup(){
    canvas.center();
    video = createCapture(VIDEO);
    video.hide();
-   classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/R8djqalPI/model.json',modelLoaded);
+   classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/-gKhJzuRM/model.json',modelLoaded);
 
 }
 
